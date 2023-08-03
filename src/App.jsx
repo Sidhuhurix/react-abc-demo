7bf43aedd1c552ab928dec457a5d7cf1c29a9f4a
sidhuhurix <sidharth.s@hurix.com>
2023-08-03 19:10:29 +0530

    Navigation to the movie details

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import "./main.css";
 //import { MovieCard } from "./MovieCard";
 import { Colorgame } from "./colorgames";
 import { Movielist } from "./movies";
 
-import { Routes, Route, Link } from "react-router-dom";
-
+import { Routes, Route, Link, useParams } from "react-router-dom";
+import { Movie } from "@mui/icons-material";
 export default function App() {
+  const MOVIE_LIST = [
+    {
+      name: "Vikram",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
+      rating: 8.4,
+      summary:
+        "Members of a black ops team must track and eliminate a gang of masked murderers.",
+    },
+    {
+      name: "RRR",
+      poster:
+        "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
+      rating: 8.8,
+      summary:
+        "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
+    },
+    {
+      name: "Iron man 2",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+      rating: 7,
+      summary:
+        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+    },
+    {
+      name: "No Country for Old Men",
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+      rating: 8.1,
+      summary:
+        "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+    },
+    {
+      name: "Jai Bhim",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+      summary:
+        "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
+      rating: 8.8,
+    },
+    {
+      name: "The Avengers",
+      rating: 8,
+      summary:
+        "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+      poster:
+        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+    },
+    {
+      name: "Interstellar",
+      poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
+      rating: 8.6,
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
+    },
+    {
+      name: "Baahubali",
+      poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
+      rating: 8,
+      summary:
+        "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+    },
+    {
+      name: "Ratatouille",
+      poster:
+        "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
+      rating: 8,
+      summary:
+        "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+    },
+    {
+      name: "PS2",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
+      summary:
+        "Ponniyin Selvan: I is an upcoming Indian Tamil-language epic period action film directed by Mani Ratnam, who co-wrote it with Elango Kumaravel and B. Jeyamohan",
+      rating: 8,
+    },
+    {
+      name: "Thor: Ragnarok",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg",
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\\n of researchers, to find a new planet for humans.",
+      rating: 8.8,
+    },
+  ];
+
+  const [Movies, setMovies] = useState(MOVIE_LIST);
   return (
     <div className="App">
       {/* <h1>Welcome to React Router!</h1> */}
@@ -15,9 +105,9 @@ export default function App() {
       {/* <Movielist /> */}
       <nav>
         <Link to="/color-game">Colorgame</Link>
-        <Link to="/sid">
-          <h1>welcome</h1>
-        </Link>
+        <p></p>
+        <Link to="/sid">welcome</Link>
+        <p></p>
         <Link to="/">Home</Link>
         <p></p>
         <Link to="/movies">Movies</Link>
@@ -25,15 +115,21 @@ export default function App() {
       <Routes>
         <Route path="/sid" element={<h1>Welcome to my page!</h1>} />
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movielist />} />
+        <Route path="/movies" element={<Movielist Movies={Movies} />} />
         <Route path="/color-game" element={<Colorgame />} />
         <Route path="*" element={<Error />} />
+        <Route path="/movies/:id" element={<MovieDetails Movies={Movies} />} />
+
         {/* <Route path="about" element={<About />} /> */}
       </Routes>
     </div>
   );
 }
 
+function MovieDetails({ Movies }) {
+  const { id } = useParams();
+  return <h2>Details- {Movies[id].name}</h2>;
+}
 function Home() {
   return (
     <div>
--- src/movies.jsx
+++ src/movies.jsx
@@ -11,102 +11,12 @@ import IconButton from "@mui/material/IconButton";
 
 import InfoIcon from "@mui/icons-material/Info";
 
-export function Movielist() {
+export function Movielist({ Movies }) {
   const [name, setname] = useState();
   const [summary, setsummary] = useState();
   const [rating, setrating] = useState();
   const [poster, setposter] = useState();
 
-  const MOVIE_LIST = [
-    {
-      name: "Vikram",
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
-      rating: 8.4,
-      summary:
-        "Members of a black ops team must track and eliminate a gang of masked murderers.",
-    },
-    {
-      name: "RRR",
-      poster:
-        "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
-      rating: 8.8,
-      summary:
-        "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
-    },
-    {
-      name: "Iron man 2",
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
-      rating: 7,
-      summary:
-        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
-    },
-    {
-      name: "No Country for Old Men",
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-      rating: 8.1,
-      summary:
-        "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
-    },
-    {
-      name: "Jai Bhim",
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-      summary:
-        "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-      rating: 8.8,
-    },
-    {
-      name: "The Avengers",
-      rating: 8,
-      summary:
-        "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-      poster:
-        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-    },
-    {
-      name: "Interstellar",
-      poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-      rating: 8.6,
-      summary:
-        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
-    },
-    {
-      name: "Baahubali",
-      poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
-      rating: 8,
-      summary:
-        "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
-    },
-    {
-      name: "Ratatouille",
-      poster:
-        "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
-      rating: 8,
-      summary:
-        "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-    },
-    {
-      name: "PS2",
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
-      summary:
-        "Ponniyin Selvan: I is an upcoming Indian Tamil-language epic period action film directed by Mani Ratnam, who co-wrote it with Elango Kumaravel and B. Jeyamohan",
-      rating: 8,
-    },
-    {
-      name: "Thor: Ragnarok",
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg",
-      summary:
-        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\\n of researchers, to find a new planet for humans.",
-      rating: 8.8,
-    },
-  ];
-
-  const [Movies, setMovies] = useState(MOVIE_LIST);
   return (
     <div className="App">
       <div>
@@ -172,18 +82,20 @@ export function Movielist() {
         </button>
       </div>
       {/* {Name}-{rating}-{summary}-{poster} */}
-      {Movies.map((x) => (
+      {Movies.map((x, index) => (
         <Moviecard
           name={x.name}
           poster={x.poster}
           summary={x.summary}
           rating={x.rating}
+          key={index}
+          id={index}
         />
       ))}
     </div>
   );
 }
-function Moviecard({ name, poster, summary, rating, moreinfo }) {
+function Moviecard({ name, poster, summary, rating, id }) {
   const [show, setShow] = useState(false);
 
   const [summary2, setsummary2] = useState(true);
@@ -192,7 +104,10 @@ function Moviecard({ name, poster, summary, rating, moreinfo }) {
     <div className="moviecard">
       <img className="pic" src={poster} />
       <div className="head">
-        <h2>{name}</h2> <h2>❤️ {rating}</h2>
+        <h2>
+          {name}-{id}
+        </h2>{" "}
+        <h2>❤️ {rating}</h2>
       </div>
       <IconButton
         onClick={() => setShow(show == true ? false : true)}
@@ -207,7 +122,7 @@ function Moviecard({ name, poster, summary, rating, moreinfo }) {
       </button> */}
       {/* /movies/0 */}
       <IconButton
-        onClick={() => navigate("/movies/0")}
+        onClick={() => navigate("/movies/" + id)}
         color="primary"
         aria-label="More info"
       >
